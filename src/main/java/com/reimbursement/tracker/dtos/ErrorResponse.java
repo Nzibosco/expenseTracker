a9390c31b493bb5b482291538f00691981ad58a3
Bosco <nzibosco@gmail.com>
2020-01-23 22:47:44 -0500

    package restructuring and gitignore file

---
+++ src/main/java/com/reimbursement/tracker/dtos/ErrorResponse.java
@@ -0,0 +1,8 @@
+package com.reimbursement.tracker.dtos;
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private int value;
+
+}
--- src/main/java/com/reimbursement/tracker/models/Role.java
+++ src/main/java/com/reimbursement/tracker/models/Role.java
@@ -1,4 +1,22 @@
 package com.reimbursement.tracker.models;
 
 public enum Role {
+    ADMIN(1, "Admin"), EMPLOYEE(2, "Employee"),
+    MANAGER(3, "Manager");
+
+    private int roleId;
+    private String name;
+
+    Role(int roleId, String name) {
+        this.roleId = roleId;
+        this.name = name;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
