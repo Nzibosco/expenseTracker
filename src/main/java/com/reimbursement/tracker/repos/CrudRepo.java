19615aa57c64b654e71c96c2a48f340ffe369abb
Bosco <nzibosco@gmail.com>
2020-01-24 00:40:25 -0500

    CRUD interface methods

--- src/main/java/com/reimbursement/tracker/repos/CrudRepo.java
+++ src/main/java/com/reimbursement/tracker/repos/CrudRepo.java
@@ -1,4 +1,13 @@
 package com.reimbursement.tracker.repos;
 
-public interface CrudRepo {
+import java.util.Optional;
+import java.util.Set;
+
+public interface CrudRepo<T> {
+
+    void save(T newObj);
+    Set<T> findAll();
+    Optional<T> findById(int id);
+    boolean update(T updatedObj);
+    boolean deleteById(int id);
 }
