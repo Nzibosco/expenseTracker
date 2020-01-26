e5210973f3c33801ad2758f0e6965300bdb0db5a
Bosco <nzibosco@gmail.com>
2020-01-26 01:23:22 -0500

    nav bars and homepage

--- src/main/java/com/reimbursement/tracker/repos/UserRepo.java
+++ src/main/java/com/reimbursement/tracker/repos/UserRepo.java
@@ -60,7 +60,7 @@ public class UserRepo implements CrudRepo<User> {
 
         try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
 
-            String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
