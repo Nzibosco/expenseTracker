e916853acbdb26219b0ef0b04288dc75b869e86a
Bosco <nzibosco@gmail.com>
2020-01-28 23:17:56 -0500

    reimb service

--- src/main/java/com/reimbursement/tracker/services/ReimbServices.java
+++ src/main/java/com/reimbursement/tracker/services/ReimbServices.java
@@ -1,4 +1,88 @@
 package com.reimbursement.tracker.services;
 
+import com.reimbursement.tracker.exceptions.InvalidRequestException;
+import com.reimbursement.tracker.exceptions.ResourceNotFoundException;
+import com.reimbursement.tracker.exceptions.ResourcePersistenceException;
+import com.reimbursement.tracker.models.Reimbursement;
+import com.reimbursement.tracker.models.User;
+import com.reimbursement.tracker.repos.ReimbRepo;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Set;
+
 public class ReimbServices {
+
+    private ReimbRepo reimbRepo;
+
+    public ReimbServices( ReimbRepo reimbRepo) {
+        this.reimbRepo = reimbRepo;
+    }
+
+    public ReimbServices() {
+        super();
+    }
+
+
+    // creating a reimbursement
+    public void registerReimb(Reimbursement newReimb) {
+
+        if (!isReimbValid(newReimb)) throw new InvalidRequestException();
+        reimbRepo.save(newReimb);
+    }
+
+    // approve or deny a reimbursement request
+    public boolean processReimb(int id, String decision, int resolver){
+        if(negativeValuesChecker(Double.valueOf(id))) return false;
+        if(negativeValuesChecker(Double.valueOf(resolver))) return false;
+        if(decision == null || decision.trim().equals("") || decision.trim() == null) return false;
+
+        boolean reimbProcessed = reimbRepo.processRequest(id, decision, resolver);
+
+        return reimbProcessed;
+    }
+
+    // get all reimbursements
+    public Set<Reimbursement> getAllReimbs(){
+        Set<Reimbursement> reimbs;
+
+        reimbs = reimbRepo.findAll();
+        if (reimbs.isEmpty()) {
+            throw new ResourceNotFoundException();
+        }
+        return reimbs;
+    }
+
+
+    // method to validate and format amount deposited or withdrawn
+    public Double validatedAmount (Double amount){
+        if(amount.isNaN() || amount < 0) return 0.0;
+
+        try{
+            // limit decimal points to 2.
+            BigDecimal toTwoDecimal = BigDecimal.valueOf(amount);
+            amount = toTwoDecimal.setScale(2, RoundingMode.DOWN).doubleValue();
+        } catch (Exception e){
+            throw new InvalidRequestException();
+        }
+        return amount;
+    }
+
+    public boolean negativeValuesChecker(Double amount){
+        if(amount <= 0){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isReimbValid(Reimbursement reimb) {
+        Double amount = reimb.getAmount();
+        Double authorId = Double.valueOf(reimb.getAuthor());
+        if (reimb == null) return false;
+        if (negativeValuesChecker(reimb.getAmount()) || amount == null || reimb.getDescription().trim().equals("")) return false;
+        if (authorId == null) return false;
+        if (Double.valueOf(reimb.getTypeId()) == null ) return false;
+        return true;
+    }
+
 }
